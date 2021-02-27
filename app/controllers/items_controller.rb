43e5e2251a21daabb59e08c2f3cdeec8d287c08c
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-28 06:48:10 +0900

    ユーザーがカード登録しているか条件分岐させる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,8 @@ class ItemsController < ApplicationController
   end
 
   def order # 購入する時のアクションを定義
+    # 購入ボタンを押した際に、「current_userに紐付くcardが存在（present）していなければ、カードの新規登録画面にリダイレクトして実行（return）する」という処理を記述
+    redirect_to new_card_path and return unless current_user.card.present?
   end
 
   private
