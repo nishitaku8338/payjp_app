abf204c4d3067773569718bc4f89133bd3d887c2
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-27 23:55:00 +0900

    カードオブジェクトを生成

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,7 +2,18 @@ const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); // 環境変数を読み込む
   const form = document.getElementById("charge-form");  // 「charge-form」とは、カード登録の入力フォームに付与されているid名
   form.addEventListener("submit", (e) => { // submitが実行されると、イベント発火
-    e.preventDefault(); // preventDefaultメソッドは、Railsのフォーム送信処理をキャンセル
+    e.preventDefault(); // preventDefaultメソッドは、Railsのフォーム送信処理をキャンセルする
+
+    // カード情報の取得先
+  const formResult = document.getElementById("charge-form");
+  const formData = new FormData(formResult);
+
+  const card = { // カードオブジェクトを生成
+      number: formData.get("number"),              // カード番号
+      cvc: formData.get("cvc"),                    // カード裏面の3桁の数字
+      exp_month: formData.get("exp_month"),        // 有効期限の月
+      exp_year: `20${formData.get("exp_year")}`,   // 有効期限の年
+    };
   });
 };
 
