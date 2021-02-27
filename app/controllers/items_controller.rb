f43a1076b886883d2aa486a34638153906466ee4
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-28 06:45:10 +0900

    idを元に、購入する商品を特定する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order  # 「find_item」を動かすアクションを限定
+
   def index
     @items = Item.all  #「Item.all」として全商品の情報を取得する
+  end
+
+  def order # 購入する時のアクションを定義
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id]) # 購入する商品を特定
   end
 end
