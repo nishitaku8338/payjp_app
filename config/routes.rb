39ae9b4743a7a5b1cfeb7ba253c172e68cabd8a7
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-27 10:44:06 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'items/index'
   devise_for :users  # deviseをインストールすると自動で追記される
   root 'items#index' # トップページになるルートパスを設定
+
+  resources :users, only: [:show, :update]
 end
