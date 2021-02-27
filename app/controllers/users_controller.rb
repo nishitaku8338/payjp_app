b77d84fd7c56dcd177558c1c1af7a88a15497718
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-28 00:14:02 +0900

    カード登録を優先するように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
     card = Card.find_by(user_id: current_user.id) # ユーザーのid情報を元に、カード情報を取得
 
+    redirect_to new_card_path and return unless card.present?  # 登録したカードが存在（present）しているかどうかで判断
+
     customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
     @card = customer.cards.first
   end
