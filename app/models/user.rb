ff01d0854513783f33eb7e9e523de024f8335a9a
nishitaku8338 <nishitaku8338@gmail.com>
2021-02-27 11:19:50 +0900

    アソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one :card, dependent: :destroy
+  # 「dependent: :destroyオプション」とは、親モデルが削除された時、それに紐付ている子モデルも一緒に削除されるというオプション
+  #  「has_one」とは、アソシエーションが１対１の時に使用する。親モデル側に「has_one」を、子モデル側に「belongs_to」を記述する
 end
